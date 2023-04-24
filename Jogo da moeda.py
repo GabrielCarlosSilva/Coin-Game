c61449fb5856f21e6de39b5c371e9c078dda8deb
Gabriel Carlos Silva <gabrielcsilva413@hotmail.com>
2023-04-24 13:58:41 -0300

    1.0

--- Jogo da moeda.py
+++ Jogo da moeda.py
@@ -2,7 +2,7 @@ import random
 import os
 
 turns=0
-maxTurns=5
+maxTurns=10
 player_play=" "
 cpu_play=" "
 cpu_points=0
@@ -10,6 +10,8 @@ player_points=0
 strategy=0
 profile=" "
 cpu_decision="___"
+last_decision="___"
+betrayed=0
 
 board=["___","___"]
 
@@ -31,26 +33,27 @@ def pTurn():
                 board[0]="_X_"
         except:
             print("Try again")
+    os.system('cls')
 
 def result():
     global turns
     global cpu_points
     global player_points
-    
-    if board[0]==board[1] and board[0]=="_X_":
-        print("Both cheated, nobody won or lost any points")
-    elif board[0]==board[1] and board[0]=="_O_":
-        print("Both coopereted, both won 2 points")
-        cpu_points+=2
-        player_points+=2
-    elif board[0]!=board[1] and board[0]=="_X_":
-        print("You betrayed you oponent, you won 3 points and they lose 1")
-        cpu_points-=1
-        player_points+=3
-    elif board[0]!=board[1] and board[0]=="_O_":
-        print("They betrayed you, you lose 1 point and they won 3 points")
-        cpu_points+=3
-        player_points-=1
+    if turns<maxTurns:
+        if board[0]==board[1] and board[0]=="_X_":
+            print("Both cheated, nobody won or lost any points")
+        elif board[0]==board[1] and board[0]=="_O_":
+            print("Both coopereted, both won 2 points")
+            cpu_points+=2
+            player_points+=2
+        elif board[0]!=board[1] and board[0]=="_X_":
+            print("You betrayed you oponent, you won 3 points and they lose 1")
+            cpu_points-=1
+            player_points+=3
+        elif board[0]!=board[1] and board[0]=="_O_":
+            print("They betrayed you, you lose 1 point and they won 3 points")
+            cpu_points+=3
+            player_points-=1
 
 def score():
     global profile
@@ -77,7 +80,7 @@ def chooser():
     global strategy
 
     if turns==0:
-        strategy=random.randrange(1,4)
+        strategy=random.randrange(1,7)
 
 def thief():
     global turns
@@ -85,9 +88,6 @@ def thief():
     global cpu_decision
     if turns<maxTurns:
         cpu_decision="_X_"
-        turns+=1
-    else:
-        turns+=1
 
 def cooperator():
     global turns
@@ -95,9 +95,6 @@ def cooperator():
     global cpu_decision
     if turns<maxTurns:
         cpu_decision="_O_"
-        turns+=1
-    else:
-        turns+=1  
 
 def chaotic():
     global turns
@@ -105,15 +102,56 @@ def chaotic():
     global cpu_play
     global cpu_decision
     if turns<maxTurns:
-        cpu_play=random.randrange(1,5)
+        cpu_play=random.randrange(1,3)
         if cpu_play==1:
             cpu_decision="_X_"
-            turns+=1
         if cpu_play==2:
             cpu_decision="_O_"
-            turns+=1
-    else:
-        turns+=1
+
+def copycat():
+    global turns
+    global maxTurns
+    global cpu_decision
+    global player_play
+    if turns<maxTurns:
+        if turns==0:
+            cpu_decision="_O_"
+        if turns>0:
+            cpu_decision=board[0]
+
+def rancorous():
+    global turns
+    global maxTurns
+    global cpu_decision
+    global betrayed
+    if board[0]=="_X_":
+        betrayed=1
+    if turns<maxTurns:
+        if betrayed==0:
+            cpu_decision="_O_"
+        elif betrayed==1:
+            cpu_decision="_X_"
+
+def detective():
+    global turns
+    global maxTurns
+    global cpu_decision
+    global betrayed
+
+    if turns<maxTurns:
+        if turns==0 or turns==2 or turns==3:
+            cpu_decision="_O_"
+            if board[0]=="_X_":
+                betrayed=1
+        elif turns==1:
+            cpu_decision="_X_"
+            if board[0]=="_X_":
+                betrayed=1
+        if turns>=4:
+            if betrayed==0:
+                cpu_decision="_X_"
+            elif betrayed==1:
+                cpu_decision=board[0]
 
 def intermediary():
     global strategy
@@ -124,15 +162,25 @@ def intermediary():
     elif strategy==2:
         thief()
         profile="Thief"
-    elif strategy==3 or strategy==4:
+    elif strategy==3:
         cooperator()
         profile="Cooperator"
+    elif strategy==4:
+        copycat()
+        profile="Copycat"
+    elif strategy==5:
+        rancorous()
+        profile="Rancorous"
+    elif strategy==6:
+        detective()
+        profile="Detective"
 
 def writer():
     global cpu_decision
+    global board
     board[1]=cpu_decision
 
-while turns<maxTurns:
+while turns<maxTurns+1:
     chooser()
     intermediary()
     pTurn()
@@ -140,3 +188,4 @@ while turns<maxTurns:
     screen()
     result()
     score()
+    turns+=1
